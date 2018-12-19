a71de121bf86340c4f84440ed3f127924c4c86a8
romankrru <rm07ru@gmail.com>
2018-12-19 20:57:01 +0300

    Redirecting requests

--- basics/app.js
+++ basics/app.js
@@ -1,7 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-	console.log(req.url, req.method, req.headers);
+	const url = req.url;
+	const method = req.method;
+
+	if (url === '/') {
+		res.write('<html>');
+		res.write('<head><title>Enter message</title></head>');
+		res.write('<body><form action="/message" method="POST"><input name="message" type="text"><button>send</button></form></body>');
+		res.write('</html>')
+		return res.end();
+	}
+
+	if (url === '/message' && method === 'POST') {
+		fs.writeFileSync('message.txt', 'DUMMY');
+		res.statusCode = 302;
+		res.setHeader('Location', '/');
+		return res.end();
+	}
 
 	res.setHeader('Content-Type', 'text/html');
 	res.write('<html>');

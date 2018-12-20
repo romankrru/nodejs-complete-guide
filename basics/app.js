7d6434b7ecf9fa8bd6200d1c39066e019cc397dc
romankrru <rm07ru@gmail.com>
2018-12-20 19:11:53 +0300

    Using express router

--- basics/app.js
+++ basics/app.js
@@ -1,15 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-	console.log('In another middleware!');
-	res.send('<h1>The "Add product"</h1>')
-});
-
-app.use('/', (req, res, next) => {
-	console.log('In another middleware!');
-	res.send('<h1>Hello from express</h1>')
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);

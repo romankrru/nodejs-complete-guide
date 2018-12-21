153a3611335371c30f9fcb36150ecec3e074f29e
romankrru <rm07ru@gmail.com>
2018-12-21 19:37:47 +0300

    Convert HTML files to pug

--- basics/routes/admin.js
+++ basics/routes/admin.js
@@ -7,7 +7,9 @@ const rootDir = require('../util/path');
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+	res.render('add-product', {
+		pageTitle: 'Add product',
+	});
 });
 
 router.post('/add-product', (req, res, next) => {

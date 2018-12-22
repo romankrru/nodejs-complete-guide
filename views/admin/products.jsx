99ebfa91f4b8202386b7cc4cdaaba56ac77655a0
romankrru <rm07ru@gmail.com>
2018-12-22 22:08:01 +0300

    Editing and deleting product

--- views/admin/products.jsx
+++ views/admin/products.jsx
@@ -1,12 +1,42 @@
 const React = require('react');
+const _  = require('lodash');
 
 const DefaultLayout = require('../layouts/default');
 
-const Products = props => <DefaultLayout
-	path={props.path}
-	pageTitle={props.pageTitle}
->
-	Admin Products page
-</DefaultLayout>;
+const AdminProducts = props => {
+	let content = <h1>No products to display</h1>;
 
-module.exports = Products;
+	if (props.prods.length) {
+		content = _.chain(props.prods)
+			.chunk(4)
+			.map((chunk, i) => <div class="row" key={i}>
+				{_.map(chunk, (product, j) => <div class="col-md-3 mb-3 mt-4" key={j}>
+					<div class="card">
+						<img src={product.imageUrl} class="card-img-top" alt=" ABook" />
+
+						<div class="card-body">
+							<h5 class="card-title">{product.title}</h5>
+							<p class="card-text">{product.description}</p>
+							<p class="card-text">${product.price}</p>
+
+							<form action="/admin/delete-product" method="POST">
+								<a href="/admin/edit-product" class="btn btn-primary mr-3">Edit</a>
+								<button type="submit" class="btn btn-danger">Delete</button>
+							</form>
+						</div>
+					</div>
+				</div>)}
+			</div>)
+			.value();
+	}
+
+
+	return <DefaultLayout
+		path={props.path}
+		pageTitle={props.pageTitle}
+	>
+		{content}
+	</DefaultLayout>
+}
+
+module.exports = AdminProducts;
--- views/shop/product-list.jsx
+++ views/shop/product-list.jsx
@@ -3,7 +3,7 @@ const _  = require('lodash');
 
 const DefaultLayout = require('../layouts/default');
 
-const Shop = props => {
+const ProductList = props => {
 	let content = <h1>No products to display</h1>;
 
 	if (props.prods.length) {
@@ -12,13 +12,17 @@ const Shop = props => {
 			.map((chunk, i) => <div class="row" key={i}>
 				{_.map(chunk, (product, j) => <div class="col-md-3 mb-3 mt-4" key={j}>
 					<div class="card">
-					<img src={product.imageUrl} class="card-img-top" alt=" ABook" />
-					<div class="card-body">
-						<h5 class="card-title">{product.title}</h5>
-						<p class="card-text">{product.description}</p>
-						<p class="card-text">${product.price}</p>
-						<a href="#" class="btn btn-primary">Add to Cart</a>
-					</div>
+						<img src={product.imageUrl} class="card-img-top" alt=" ABook" />
+
+						<div class="card-body">
+							<h5 class="card-title">{product.title}</h5>
+							<p class="card-text">{product.description}</p>
+							<p class="card-text">${product.price}</p>
+
+							<form action="/add-to-cart" method="POST">
+								<button href="#" class="btn btn-primary">Add to Cart</button>
+							</form>
+						</div>
 					</div>
 				</div>)}
 			</div>)
@@ -34,4 +38,4 @@ const Shop = props => {
 	</DefaultLayout>
 }
 
-module.exports = Shop;
+module.exports = ProductList;

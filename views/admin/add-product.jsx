327541e0c214625e0172567895593a21cb4837a1
romankrru <rm07ru@gmail.com>
2018-12-22 21:53:36 +0300

    Storing Product data

--- views/admin/add-product.jsx
+++ views/admin/add-product.jsx
@@ -6,10 +6,27 @@ const AddProduct = props => <DefaultLayout
 	pageTitle={props.pageTitle}
 >
 	<main>
+		<h1>Add product</h1>
+
 		<form action="/admin/add-product" method="POST">
 			<div class="form-group">
 				<label htmlFor="title">Book Title</label>
 				<input type="text" class="form-control" id="title" name="title" />
+			</div>
+
+			<div class="form-group">
+				<label htmlFor="imageUrl">Image Url</label>
+				<input type="text" class="form-control" id="imageUrl" name="imageUrl" />
+			</div>
+
+			<div class="form-group">
+				<label htmlFor="Price">Price</label>
+				<input type="number" class="form-control" id="price" name="price" />
+			</div>
+
+			<div class="form-group">
+				<label htmlFor="Description">Description</label>
+				<textarea type="number" class="form-control" id="description" name="description" rows={5}></textarea>
 			</div>
 
 			<button type="submit" class="btn btn-primary mb-2">Add product</button>

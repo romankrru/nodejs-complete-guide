c81a66b4779bb36d6d4274f104b65f100c704587
romankrru <rm07ru@gmail.com>
2018-12-22 22:33:36 +0300

    Extracting Dynamic Params

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -7,7 +7,7 @@ const DefaultLayout = props => <html lang="en">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 		<title>{props.pageTitle}</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"></link>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossOrigin="anonymous"></link>
 		<link rel="stylesheet" href="/css/main.css" />
 
 		{(props.styles || []).map(href => <link rel="stylesheet" href={href} key={href}/>)}
--- views/shop/product-list.jsx
+++ views/shop/product-list.jsx
@@ -9,20 +9,20 @@ const ProductList = props => {
 	if (props.prods.length) {
 		content = _.chain(props.prods)
 			.chunk(4)
-			.map((chunk, i) => <div class="row" key={i}>
-				{_.map(chunk, (product, j) => <div class="col-md-3 mb-3 mt-4" key={j}>
-					<div class="card">
-						<img src={product.imageUrl} class="card-img-top" alt=" ABook" />
+			.map((chunk, i) => <div className="row" key={i}>
+				{_.map(chunk, (product, j) => <div className="col-md-3 mb-3 mt-4" key={j}>
+					<div className="card">
+						<img src={product.imageUrl} className="card-img-top" alt=" ABook" />
 
-						<div class="card-body">
-							<h5 class="card-title">{product.title}</h5>
-							<p class="card-text">{product.description}</p>
-							<p class="card-text">${product.price}</p>
+						<div className="card-body">
+							<h5 className="card-title">{product.title}</h5>
+							<p className="card-text">{product.description}</p>
+							<p className="card-text">${product.price}</p>
 
 							<a href={`/products/${product.id}`}>Details</a>
 
 							<form action="/add-to-cart" method="POST">
-								<button href="#" class="btn btn-primary">Add to Cart</button>
+								<button href="#" className="btn btn-primary">Add to Cart</button>
 							</form>
 						</div>
 					</div>

e85be842395be58579b1f8fd98af33660c63b9be
romankrru <rm07ru@gmail.com>
2018-12-23 18:58:21 +0300

    Add favicon

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -5,6 +5,7 @@ const DefaultLayout = props => <html lang="en">
 	<head>
 		<meta charSet="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="shortcut icon" href="/favicon.png" type="image/png" />
 		<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 		<title>{props.pageTitle}</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossOrigin="anonymous"></link>

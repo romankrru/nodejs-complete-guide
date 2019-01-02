18ed583ce75406acb272c24aeb4dfa0fd16f5f39
romankrru <rm07ru@gmail.com>
2019-01-02 15:07:00 +0300

    Inserting data into the database

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,9 +6,16 @@ exports.postAddProduct = (req, res) => {
 	const price = req.body.price;
 	const description = req.body.description;
 
-	const product = new Product(title, imageUrl, description, price);
-	product.save();
-	res.redirect('/');
+	const product = new Product(
+		title,
+		imageUrl,
+		description,
+		price,
+	);
+
+	product.save()
+		.then(() => res.redirect('/'))
+		.catch(err => console.error(err));
 };
 
 exports.getAddProduct = (req, res) => {

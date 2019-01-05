a9a3c92b28961a51f9ff8c3be8197ad56b5f8715
romankrru <rm07ru@gmail.com>
2019-01-05 11:54:31 +0300

    Adding one-to-many relations

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,7 +23,15 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, {
+	constraints: true,
+	onDelete: 'CASCADE',
+});
+
+User.hasMany(Product);
+
+sequelize
+	.sync({force: true})
 
 	.then(() => {
 		app.listen(3000);

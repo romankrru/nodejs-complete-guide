1962ac7be746a6f890d714ae187ec45074fe2449
romankrru <rm07ru@gmail.com>
2019-01-20 14:49:00 +0300

    Add addToCart method

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const fp = require('lodash/fp');
 
 const userSchema = new Schema({
 	cart: {
@@ -27,5 +28,31 @@ const userSchema = new Schema({
 		type: String,
 	},
 });
+
+userSchema.methods.addToCart = function(product) {
+	const idx = fp.findIndex(
+		fp.flow(
+			fp.get('productId'),
+			fp.concat(product._id),
+			fp.map(String),
+			fp.apply(fp.eq),
+		),
+
+		this.cart.items,
+	);
+
+	if (idx > -1) {
+		// update qty
+		this.cart.items[idx].quantity = this.cart.items[idx].quantity + 1;
+	} else {
+		// push new product
+		this.cart.items.push({
+			productId: product._id,
+			quantity: 1,
+		});
+	}
+
+	return this.save();
+};
 
 module.exports = mongoose.model('User', userSchema);

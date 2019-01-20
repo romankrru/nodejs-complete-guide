c413892b11f3b725910bc5590aa6be358287d0df
romankrru <rm07ru@gmail.com>
2019-01-20 12:11:48 +0300

    Creating the product schema

--- models/product.js
+++ models/product.js
@@ -1,3 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+	description: {
+		required: true,
+		type: String,
+	},
+
+	imageUrl: {
+		required: true,
+		type: String,
+	},
+
+	price: {
+		required: true,
+		type: Number,
+	},
+
+	title: {
+		required: true,
+		type: String,
+	},
+});
+
+module.exports = productSchema;
+
 // const {ObjectID} = require('mongodb');
 
 // const {getDb} = require('../util/database');

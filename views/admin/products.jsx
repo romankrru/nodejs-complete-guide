511c49238daaaf44a96a9ade868d7abc1dcfd5d4
romankrru <rm07ru@gmail.com>
2019-01-28 10:51:42 +0300

    Fix views

--- views/admin/products.jsx
+++ views/admin/products.jsx
@@ -1,44 +1,44 @@
 const React = require('react');
-const _  = require('lodash');
+const fp = require('lodash/fp');
 
 const DefaultLayout = require('../layouts/default');
 const CSRFInput = require('../common/CSRFInput');
+
+const renderProductsRow = (row, i) => <div className="row" key={i}>
+	{fp.map(product => <div className="col-md-3 mb-3 mt-4" key={product._id}>
+		<div className="card">
+			<img src={product.imageUrl} className="card-img-top" alt=" ABook" />
+
+			<div className="card-body">
+				<h5 className="card-title">{product.title}</h5>
+				<p className="card-text">{product.description}</p>
+				<p className="card-text">${product.price}</p>
+
+				<form action="/admin/delete-product" method="POST">
+					<a
+						href={`/admin/edit-product/${product._id}`}
+						className="btn btn-primary mr-3"
+					>
+						Edit
+					</a>
+
+					<CSRFInput />
+					<input type="hidden" value={product._id} name="productId" />
+					<button type="submit" className="btn btn-danger">Delete</button>
+				</form>
+			</div>
+		</div>
+	</div>, row)}
+</div>;
 
 const AdminProducts = props => {
 	let content = <h1>No products to display</h1>;
 
 	if (props.prods.length) {
-		content = _.chain(props.prods)
-			.chunk(4)
-			.map((chunk, i) => <div className="row" key={i}>
-				{_.map(chunk, (product, j) => <div className="col-md-3 mb-3 mt-4" key={j}>
-					<div className="card">
-						<img src={product.imageUrl} className="card-img-top" alt=" ABook" />
-
-						<div className="card-body">
-							<h5 className="card-title">{product.title}</h5>
-							<p className="card-text">{product.description}</p>
-							<p className="card-text">${product.price}</p>
-
-							<form action="/admin/delete-product" method="POST">
-								<a
-									href={`/admin/edit-product/${product._id}`}
-									className="btn btn-primary mr-3"
-								>
-									Edit
-								</a>
-
-								<CSRFInput />
-								<input type="hidden" value={product._id} name="productId" />
-								<button type="submit" className="btn btn-danger">Delete</button>
-							</form>
-						</div>
-					</div>
-				</div>)}
-			</div>)
-			.value();
+		content = props.prods
+			|> fp.chunk(4)
+			|> fp.map(renderProductsRow);
 	}
-
 
 	return <DefaultLayout
 		path={props.path}
--- views/shop/product-list.jsx
+++ views/shop/product-list.jsx
@@ -1,37 +1,36 @@
 const React = require('react');
-const _  = require('lodash');
+const fp = require('lodash/fp');
 const {LocalsContext} = require('express-react-static-markup');
 
 const DefaultLayout = require('../layouts/default');
 const AddToCart = require('../common/AddToCart');
 
+const renderProductsRow = (row, i) => <div className="row" key={i}>
+	{fp.map(product => <div className="col-md-3 mb-3 mt-4" key={product._id}>
+		<div className="card">
+			<img src={product.imageUrl} className="card-img-top" alt=" ABook" />
+
+			<div className="card-body">
+				<h5 className="card-title">{product.title}</h5>
+				<p className="card-text">{product.description}</p>
+				<p className="card-text">${product.price}</p>
+				<a href={`/products/${product._id}`}>Details</a>
+
+				<LocalsContext>
+					{({isLoggedIn}) => isLoggedIn && <AddToCart productId={product._id} />}
+				</LocalsContext>
+			</div>
+		</div>
+	</div>, row)}
+</div>;
+
 const ProductList = props => {
 	let content = <h1>No products to display</h1>;
 
 	if (props.prods.length) {
-		content = _.chain(props.prods)
-			.chunk(4)
-
-			.map((chunk, i) => <div className="row" key={i}>
-				{_.map(chunk, (product, j) => <div className="col-md-3 mb-3 mt-4" key={j}>
-					<div className="card">
-						<img src={product.imageUrl} className="card-img-top" alt=" ABook" />
-
-						<div className="card-body">
-							<h5 className="card-title">{product.title}</h5>
-							<p className="card-text">{product.description}</p>
-							<p className="card-text">${product.price}</p>
-							<a href={`/products/${product._id}`}>Details</a>
-
-							<LocalsContext>
-								{({isLoggedIn}) => isLoggedIn && <AddToCart productId={product._id} />}
-							</LocalsContext>
-						</div>
-					</div>
-				</div>)}
-			</div>)
-
-			.value();
+		content = props.prods
+			|> fp.chunk(4)
+			|> fp.map(renderProductsRow);
 	}
 
 
@@ -41,6 +40,6 @@ const ProductList = props => {
 	>
 		{content}
 	</DefaultLayout>
-}
+};
 
 module.exports = ProductList;

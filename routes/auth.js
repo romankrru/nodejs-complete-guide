f13d4bc5763900289cbc67836bb9f33fe255b27c
romankrru <rm07ru@gmail.com>
2019-02-10 16:10:28 +0300

    Custom validators

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,24 @@ router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignup);
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.'), authController.postSignup);
+
+router.post(
+	'/signup',
+
+	check('email')
+		.isEmail()
+		.withMessage('Please enter a valid email.')
+
+		.custom(value => {
+			if(value === 'test@test1.com')
+				throw new Error('This email address is forbidden.');
+
+			return true;
+		}),
+
+	authController.postSignup
+);
+
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
 router.get('/reset/:token', authController.getNewPassword);

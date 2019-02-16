fd790267766f0782f3064d1b1f841c0507e8a138
romankrru <rm07ru@gmail.com>
2019-02-16 18:11:30 +0300

    1

--- util/handleError.js
+++ util/handleError.js
@@ -3,10 +3,11 @@ const fp = require('lodash/fp');
 
 const log = a => (console.log(a), a);
 
-const handleError = next => err => err
-	|> log
-	|> new Error(_)
-	|> fp.set('httpStatusCode', '500')
-	|> next;
+const handleError = next => fp.flow(
+	log,
+	new Error(_),
+	fp.set('httpStatusCode', '500'),
+	next,
+);
 
 module.exports = handleError;
